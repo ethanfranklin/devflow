c84f6f75a6bd394ee827f5a6a16d60e804e6c3d9
Ethan Franklin <ethanpaulfranklin@gmail.com>
2024-11-04 15:55:19 -0500

    Tailwind Setup and Theming

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-      <h1 className="text-3xl font-black text-violet-700">Welcome to the world of Next.js</h1>
+      <h1 className="h1-bold">Welcome to the world of Next.js</h1>
     </>
   );
 }
